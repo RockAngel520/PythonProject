c1bae5ab8432605d830bc1e6320bbb42494ba5a0
Anton-Z <rockangel520@mail.ru>
2025-05-09 19:12:07 +0500

    test

---
+++ src/11.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=1"
+
+payload = {}
+headers= {
+  "apikey": "yTQXbpvWrKiapyBvQDW5cmkT4D9WtWoD"
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+status_code = response.status_code
+result = response.text
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,8 @@
 import json
+import requests
+
+
+API_KEY = 6TnEwuTZ6DS9llgpXGHy2QFvDQvZTwcfм
 
 def read_json_file(path: str) -> list[dict | None]:
     '''
@@ -11,5 +15,19 @@ def read_json_file(path: str) -> list[dict | None]:
          return []
 
 
+
+
+
+def currency_conversions(transaction: dict) -> float:
+    '''Функция, которая возвращает сумму транзакции в рублях'''
+    if transaction['operationAmount']['currency']['code'] == 'RUB':
+        amount = transaction['operationAmount']['amount']
+    else:
+        pass
+
+    return amount
+
+
 if __name__ == '__main__':
     print(read_json_file('data/operations.json'))
+    print(currency_conversions(read_json_file('data/operations.json')[1]))
