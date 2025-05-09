995b0bdf8a31106a2ce0ed336e478ef129cba1f4
Anton <rockangel520@mail.ru>
2025-05-09 21:17:48 +0500

    done read_json_file in external_api.py and .env.example

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,5 @@
 import json
-import requests
 
-
-API_KEY = 6TnEwuTZ6DS9llgpXGHy2QFvDQvZTwcfм
 
 def read_json_file(path: str) -> list[dict | None]:
     '''
@@ -15,19 +12,5 @@ def read_json_file(path: str) -> list[dict | None]:
          return []
 
 
-
-
-
-def currency_conversions(transaction: dict) -> float:
-    '''Функция, которая возвращает сумму транзакции в рублях'''
-    if transaction['operationAmount']['currency']['code'] == 'RUB':
-        amount = transaction['operationAmount']['amount']
-    else:
-        pass
-
-    return amount
-
-
-if __name__ == '__main__':
-    print(read_json_file('data/operations.json'))
-    print(currency_conversions(read_json_file('data/operations.json')[1]))
+# if __name__ == '__main__':
+#     print(read_json_file('data/operations.json'))
