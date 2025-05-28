f108cf8d447dc7c6b50a45f7218d05aec93cdfe0
Anton <rockangel520@mail.ru>
2025-05-29 00:40:04 +0500

    done test_searches.py

--- src/searches.py
+++ src/searches.py
@@ -15,14 +15,39 @@ def count_categories(transactions: list[dict], categories: list) -> dict:
 
 
 # my_transactions = [
-# {'id': 2177828.0, 'state': 'EXECUTED', 'date': '2022-04-14T15:14:21Z', 'amount': 24853.0, 'currency_name': 'Yuan Renminbi',
-# 'currency_code': 'CNY', 'from': 'Счет 38577962752140632721', 'to': 'Счет 47657753885349826314',
-# 'description': 'Перевод со счета на счет'},
-#
-# {'id': 4699552.0, 'state': 'EXECUTED', 'date': '2022-03-23T08:29:37Z', 'amount': 23423.0, 'currency_name': 'Peso',
-# 'currency_code': 'PHP', 'from': 'Discover 7269000803370165', 'to': 'American Express 1963030970727681',
-# 'description': 'Перевод с карты на карту'}
-# ]
+#         {
+#             "id": 4699552.0,
+#             "state": "EXECUTED",
+#             "date": "2022-03-23T08:29:37Z",
+#             "amount": 23423.0,
+#             "currency_name": "Peso",
+#             "currency_code": "PHP",
+#             "from": "Discover 7269000803370165",
+#             "to": "American Express 1963030970727681",
+#             "description": "Перевод с карты на карту",
+#         },
+#         {
+#             'id': 2177828.0,
+#             'state': 'EXECUTED',
+#             'date': '2022-04-14T15:14:21Z',
+#             'amount': 24853.0,
+#             'currency_name': 'Yuan Renminbi',
+#             'currency_code': 'CNY',
+#             'from': 'Счет 38577962752140632721',
+#             'to': 'Счет 47657753885349826314',
+#             'description': 'Перевод со счета на счет'},
+#         {
+#             'id': 214410.0,
+#             'state': 'EXECUTED',
+#             'date': '2020-10-29T09:56:28Z',
+#             'amount': 34336.0,
+#             'currency_name': 'Krona',
+#             'currency_code': 'SEK',
+#             'from': 'Discover 6590095029387674',
+#             'to': 'Mastercard 0974688087552673',
+#             'description': 'Перевод с карты на карту'
+#         },
+#     ]
 # categories = ['Перевод с карты на карту', 'Перевод со счета на счет']
 # my_string = 'СЧЕТ'
 #
